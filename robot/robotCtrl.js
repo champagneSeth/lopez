f4666ac907eeb3395b48f6d26e820ef3d8054ac1
Isabella Bendana <Isabella Bendana>
2016-04-24 20:10:47 -0500

    This is the motor pins working, hopefully..

--- robot/robotCtrl.js
+++ robot/robotCtrl.js
@@ -2,6 +2,7 @@ var register    = require('../sonus/softCtrl.js').register
 ,   Gpio        = require('onoff')
 ;
 
+
 var definition = {
     deviceName  : 'lopez',
     commands    : {
@@ -12,11 +13,39 @@ var definition = {
     }
 }
 
+var rf1 = new Gpio (20, 'out');
+var rf2 = new Gpio (16, 'out');
+var rfe = new Gpio (26, 'out');
+
+var rb1 = new Gpio (24, 'out');
+var rb2 = new Gpio (23, 'out');
+var rbe = new Gpio (25, 'out');
+
+var lf1 = new Gpio (19, 'out');
+var lf2 = new Gpio (13, 'out');
+var lfe = new Gpio (26, 'out');
+
+var lb1 = new Gpio (19, 'out');
+var lb2 = new Gpio (13, 'out');
+var lbe = new Gpio (26, 'out');
+
+rfe.write(1, check);
+rbe.write(1, check);
+lfe.write(1, check);
+lbe.write(1, check);
+
+
 module.exports = {
     execute : robotMove,
-    device  : register(definition)    
+    device  : register(definition),
+    shutdown : unexport   
 }
 
+function check(error) {
+    if (error) {
+        console.log(error)
+    }
+}
 
 function robotMove(command, callBack) {
     console.log('[ robot ] Robot do something: ' + command);
@@ -37,7 +66,72 @@ function robotMove(command, callBack) {
 }
 
 function girarDerecha() {
-    console.log('woo')
+    rf1.write(1, check);
+    lf2.write(1, check);
+    rb1.write(1, check);
+    lb2.write(1, check);
+    setTimeout(function () {
+        rf1.write(0, check);
+        lf2.write(0, check);
+        rb1.write(0, check);
+        lb2.write(0, check);
+    }, 2000);
 }
 
+function girarIzquierda() {
+    rf2.write(1, check);
+    lf1.write(1, check);
+    rb2.write(1, check);
+    lb1.write(1, check);
+    setTimeout(function () {
+        rf2.write(0, check);
+        lf1.write(0, check);
+        rb2.write(0, check);
+        lb1.write(0, check);
+    }, 2000);
+}
+
+function irAdelante() {
+    rf1.write(1, check);
+    lf1.write(1, check);
+    rb1.write(1, check);
+    lb1.write(1, check);
+    setTimeout(function () {
+        rf1.write(0, check);
+        lf1.write(0, check);
+        rb1.write(0, check);
+        lb1.write(0, check);
+    }, 2000);
+}
+
+function irAtras() {
+    rf2.write(1, check);
+    lf2.write(1, check);
+    rb2.write(1, check);
+    lb2.write(1, check);
+    setTimeout(function () {
+        rf2.write(0, check);
+        lf2.write(0, check);
+        rb2.write(0, check);
+        lb2.write(0, check);
+    }, 2000);
+}
+
+function unexport() {
+    rf1.unexport();
+    rf2.unexport();
+    rfe.unexport();
+
+    rb1.unexport();
+    rb2.unexport();
+    rbe.unexport();
+
+    lf1.unexport();
+    lf2.unexport();
+    lfe.unexport();
+
+    lb1.unexport();
+    lb2.unexport();
+    lbe.unexport();
+}
 
